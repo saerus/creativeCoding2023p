10949f18cffc8fc2f2aee6623704f88b0edf672e
Marc Dubois <marc@fragment.in>
2023-03-03 11:08:17 +0100

    T2

--- T2/index.js
+++ T2/index.js
@@ -1,5 +1,12 @@
+let gridQttX = 15;
+let gridQttY =10;
+let columnWidth;
+let rowHeight;
+let img;
+
 // P5JS preload
 function preload() {
+    img = loadImage("assets/imgB.png");
 }
 // user input
 function mousePressed() {
@@ -8,16 +15,22 @@ function mousePressed() {
 function setup() {
     createCanvas(windowWidth, windowHeight, P2D);
     pixelDensity(1);
+    //
+    columnWidth = round(width/gridQttX);
+    rowHeight = round(height/gridQttY);
+    //
     background(255);
     rectMode(CENTER);
     imageMode(CENTER);
+    // blendMode(EXCLUSION);
     //
 }
 // P5JS draw
 function draw() {
     if (mouseIsPressed) {
         let a = getRandomPoint();
-        let b = getRandomPoint();
+        let b = a.copy();
+        b.x+=columnWidth;
         // b.y+=rowHeight;
         drawFromTo(a, b);
     }
@@ -25,6 +38,8 @@ function draw() {
 // P5JS windowResized
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+    columnWidth = round(width/gridQttX);
+    rowHeight = round(height/gridQttY);
 }
 // Draw arrow from A to B
 function drawFromTo(a, b) {
@@ -32,12 +47,36 @@ function drawFromTo(a, b) {
     translate(a.x, a.y);
     let dir = b.sub(a);
     rotate(dir.heading());
-    scale(dir.mag());
+    scale(columnWidth, rowHeight);
     strokeWeight(1 / dir.mag());
-    // ICI
-    line(0, 0, 1, 0);
-    line(1, 0, 0.99, -0.01);
-    line(1, 0, 0.99, 0.01);
+    fill(0);
+    // stroke(255, 0, 0);
+    // line(0, 0, 1, 0);
+    // line(1, 0, 0.99, -0.01);
+    // line(1, 0, 0.99, 0.01);
+    noStroke();
+    push();
+    translate(0.5, 0.5);
+    if(random(0, 1) < 0.5) {
+        rotate(PI/2);
+    }
+    translate(-0.5, -0.5);
+    fill(255);
+    // ellipse(0.5, 0.5, 1, 1);
+    fill(0);
+    //
+    //
+    image(img, 0.5, 0.5, 1, 1);
+    // beginShape();
+    // vertex(0, 0);
+    // vertex(0.5, 0);
+    // vertex(1, 0.5);
+    // vertex(1, 1);
+    // vertex(0.5, 1);
+    // vertex(0, 0.5);
+    // endShape(CLOSE);
+    //
+    pop();
     //
     pop();
 }
@@ -50,12 +89,8 @@ function getRandomPoint() {
 }
 
 function gridifyX(n) {
-    let gridQttX = 10;
-    let columnWidth = width/gridQttX;
     return round(n/columnWidth)*columnWidth;
 }
 function gridifyY(n) {
-    let gridQttY = 5;
-    let rowHeight = height/gridQttY;
     return round(n/rowHeight)*rowHeight;
 }
